04aedd8f6964ee3a639a9d47dd6915e443d3a7cc
tugudush <jerome2kph@gmail.com>
2024-07-12 13:50:03 +0800

    page not found

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <>
+      <main className="not-found">
+        <p>Unfortunately, we could not find the requested page or resource.</p>
+      </main>
+    </>
+  );
+}
