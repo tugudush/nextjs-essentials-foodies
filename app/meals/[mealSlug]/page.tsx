d7444c9dce3242fd2913f72db103c1b879836498
tugudush <jerome2kph@gmail.com>
2024-08-23 16:29:18 +0800

    fetchting db from sqlite

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,13 +1,51 @@
-interface MealProps {
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+interface PageProps {
   params: {
-    slug: string;
+    mealSlug: string;
   };
 }
 
-export default function MealDetailsPage({ params }: MealProps) {
+interface MealSlugProps {
+  id: number;
+  slug: string;
+  title: string;
+  image: string;
+  summary: string;
+  instructions: string;
+  creator: string;
+  creator_email: string;
+}
+
+export default function MealDetailsPage({ params }: PageProps) {
+  console.log("slug: ", params.mealSlug);
+  const meal = getMeal(params.mealSlug) as MealSlugProps;
+  console.log("meal title: ", meal.title);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
     <>
-      <h1>{params.slug}</h1>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
     </>
   );
 }
--- lib/meals.ts
+++ lib/meals.ts
@@ -6,3 +6,7 @@ export async function getMeals() {
   //throw new Error("Loading meals failed");
   return db.prepare("SELECT * FROM meals").all();
 }
+
+export function getMeal(slug: string) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+}
